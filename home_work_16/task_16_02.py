11c742c47b55cc35529cc3bf67a788093404d28c
Kris <ivanov.kristian2000@gmail.com>
2026-05-14 20:03:30 +0200

    Modifid home work

--- home_work_16/task_16_02.py
+++ home_work_16/task_16_02.py
@@ -34,6 +34,10 @@ for char in user_input:
     if char in sample:
         stak.append(char)
     else:
+        if not stak:
+            res = False
+            break
+
         last = stak.pop()
         if last == '[' and char == ']':
             res = True
@@ -52,3 +56,4 @@ if stak:
     res = False
 
 print(user_input,':', res)
+
